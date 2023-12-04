95be7a292585d44884f1fa227fd4183281535579
prince ajuzie <princeajuzie1@gmail.com>
2023-12-04 15:30:39 +0100

    fix

--- src/app/bookmark/page.tsx
+++ src/app/bookmark/page.tsx
@@ -1,4 +1,4 @@
-"use client"
+"use client";
 import Footer from "@/components/FooterComp/Footer";
 import Leftsidebar from "@/components/LeftsidebarComp/LeftSidebar";
 import Navbar from "@/components/NavbarComp/Navbar";
@@ -6,84 +6,70 @@ import { useEffect, useState } from "react";
 import Link from "next/link";
 
 export default function Page() {
-
   interface Data {
-    id: number,
-    name: string,
-    poster_path: string,
+    id: number;
+    name: string;
+    poster_path: string;
   }
 
-  const [data, setData] = useState<Data[] | null>(null)
-  const [loading, setLoading] = useState<boolean>(true)
+  const [data, setData] = useState<Data[] | null>(null);
+  const [loading, setLoading] = useState<boolean>(true);
 
-  useEffect(()=>{
-    
-      const savedBookmarks = JSON.parse(localStorage.getItem('bookmark') ?? "[]") || [];
-    if(savedBookmarks){
-      setData(savedBookmarks)
-      setLoading(false)
-      
+  useEffect(() => {
+    const savedBookmarks =
+      JSON.parse(localStorage.getItem("bookmark") ?? "[]") || [];
+    if (savedBookmarks) {
+      setData(savedBookmarks);
+      setLoading(false);
     }
-      
-    },[])
+  }, []);
 
-    function name(params:any) {
-      
-    }
   return (
     <>
       <div>
-      <div>
-        <Navbar />
-      </div>
-
-      <div>
-        <Leftsidebar />
-      </div>
-
-      <div className=" px-[1rem] md:pt-[5rem] lg:pl-[7rem] pt-[5rem] lg:pr-[1rem] h-[100vh]">
-        { loading ? 
-
-     
-     
-        
-           <div className="h-[50%] w-auto">
-            <h1 className="flex items-center justify-center text-red-600 text-center">No BookMark found</h1>
-           </div>
-   
-
-   :  
+        <div>
+          <Navbar />
+        </div>
 
         <div>
-          <ul className=" grid-cols-2 md:grid-col-3 grid lg:grid-cols-4  gap-[1rem]">
-            {data && data.map((item)=>{
-
-              return (
-                <Link href={`/${item.id}`} key={item.id}>
-            
-
-                <li  className="  "> 
-                <img
-                        src={`https://www.themoviedb.org/t/p/w440_and_h660_face/${item.poster_path}`}
-                        alt=""
-                        className="rounded-lg  mb-[] h-[auto] w-[16rem]"
-                      />
-                </li>
-                </Link>
-             )
-            })
-
-            }
-            
-          </ul>
+          <Leftsidebar />
         </div>
-     
-        }
+
+        <div className=" px-[1rem] md:pt-[5rem] lg:pl-[7rem] pt-[5rem] lg:pr-[1rem] h-[100%]">
+          {loading ? (
+            <></>
+          ) : (
+            <div>
+              {data?.length === 0? (
+                <div className="h-[60vh] w-auto flex items-center justify-center m-auto">
+                  <h1 className=" text-red-600 text-center">
+                    No BookMark found
+                  </h1>
+                </div>
+              ) : (
+                <ul className=" grid-cols-2 md:grid-col-3 grid lg:grid-cols-4  gap-[1rem]">
+                  {data?.map((item) => {
+                    return (
+                      <Link href={`/${item.id}`} key={item.id}>
+                        <li className="  ">
+                          <img
+                            src={`https://www.themoviedb.org/t/p/w440_and_h660_face/${item.poster_path}`}
+                            alt=""
+                            className="rounded-lg  mb-[] h-[auto] w-[16rem]"
+                          />
+                        </li>
+                      </Link>
+                    );
+                  })}
+                </ul>
+              )}
+            </div>
+          )}
+        </div>
       </div>
       <Footer />
-    </div>
     </>
-  )
+  );
 }
 
 //
