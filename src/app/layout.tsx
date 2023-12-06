8448c2b4e3c9493ded7c6baab06fb000da090e40
prince ajuzie <princeajuzie1@gmail.com>
2023-12-06 19:45:07 +0100

    update

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import './globals.css'
 import { CollapseProvider } from '@/context/CollapseContext'
+import { Analytics } from '@vercel/analytics/react';
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -19,7 +20,7 @@ export default function RootLayout({
     <html lang="en">
       <CollapseProvider>
       
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>{children}   <Analytics /></body>
       </CollapseProvider>
     </html>
   )
